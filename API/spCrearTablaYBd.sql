3f402026f816f1b975fbad1c57764fb7cafde024
Raul Alberto Corral Lara <raulcorral20@gmail.com>
2024-07-15 00:33:23 -0600

    Agregar spCrearTablaYBd

---
+++ API/spCrearTablaYBd.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE BdiExamen;
+GO
+USE BdiExamen;
+
+CREATE TABLE tblExamen(
+	idExamen INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(255) NOT NULL,
+	Descripcion VARCHAR(255) NOT NULL
+);
